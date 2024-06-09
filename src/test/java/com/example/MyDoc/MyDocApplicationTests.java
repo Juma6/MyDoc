39d6ff2c5e4bffd7e78b218b4c573c56bbf57f65
JUMAA <ndegwajumaa43@gmail.com>
2024-06-09 17:47:36 +0300

    my first commit on MyDoc chat bot

---
+++ src/test/java/com/example/MyDoc/MyDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
