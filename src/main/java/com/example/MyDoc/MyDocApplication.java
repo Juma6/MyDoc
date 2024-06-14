f27b589a49ae4629317694bcb0a99ee41ad3c6db
JUMAA <ndegwajumaa43@gmail.com>
2024-06-14 10:49:57 +0300

    updates

--- src/main/java/com/example/MyDoc/MyDocApplication.java
+++ src/main/java/com/example/MyDoc/MyDocApplication.java
@@ -2,6 +2,9 @@ package com.example.MyDoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger.readers.parameter.SwaggerExpandedParameterBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class MyDocApplication {
@@ -9,5 +12,6 @@ public class MyDocApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyDocApplication.class, args);
 	}
+
 
 }
